f9db57743fe1f4f90d965cdc346bda73d5652e82
Utso Roy <85325828+utso185-h@users.noreply.github.com>
2023-01-16 22:36:27 +0600

    Add files via upload

---
+++ Lab_2/3. 244_50_07.cpp
@@ -0,0 +1,18 @@
+//codeforce problem no- 231A
+//problem name - Team
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int count = 0, p, v, t, n;
+    cin>>n;
+    while(n){
+        cin>>p>>v>>t;
+        if(p+v+t>=2){
+            count++;
+        }
+        n--;
+    }
+    cout<<count<<endl;
+}

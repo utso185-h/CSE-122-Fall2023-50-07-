41eb9e951c6edfd98763be49b9ae0d1e7c242afe
Utso Roy <85325828+utso185-h@users.noreply.github.com>
2023-02-14 09:15:30 +0600

    Add files via upload

---
+++ lab_7/11.244_50_07.cpp
@@ -0,0 +1,28 @@
+//Problem Number: Codeforces 118A
+//problem Name: String Task
+
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+  string s;
+  cin>>s;
+  //int a=s.length();
+  for(int i=0;i<s.size();i++)
+  {
+     s[i]=tolower(s[i]);
+     if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='y')
+     {
+        continue;
+     }
+     else
+    {
+       cout<<"."<<s[i];
+     }
+
+  }
+ return 0;
+}
